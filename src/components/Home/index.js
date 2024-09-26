12a6dcf591c0a6f513ee271e4f546e6a5645695b
root <bhargavareddy2005@email.com>
2024-09-26 15:20:06 +0000

    sdf

---
+++ src/components/Home/index.js
@@ -0,0 +1,12 @@
+import Header from '../Header'
+// Write your code here
+const Home = () => (
+  <div>
+    <Header />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/home-sm-img.png"
+      alt="home"
+    />
+  </div>
+)
+export default Home
